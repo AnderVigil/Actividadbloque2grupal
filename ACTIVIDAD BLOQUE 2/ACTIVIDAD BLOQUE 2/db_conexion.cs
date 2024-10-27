2679d24f1511fab36510889512bb683b41657cb6
AnderVigil <andervigil55@gmail.com>
2024-10-26 21:34:18 -0600

    Avance 1

---
+++ ACTIVIDAD BLOQUE 2/ACTIVIDAD BLOQUE 2/db_conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ACTIVIDAD_BLOQUE_2
+{
+    internal class db_conexion
+    {
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand miComando = new SqlCommand();
+        SqlDataAdapter miDataAdapter = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Peliculas (
+    Id INT PRIMARY KEY IDENTITY(1,1), -- Clave primaria, auto-incremental
+    Titulo NVARCHAR(100) NOT NULL,    -- Título de la película
+    Autor NVARCHAR(100) NOT NULL,     -- Autor o director
+    Sinopsis NVARCHAR(MAX) NULL,      -- Sinopsis de la película
+    Duracion INT NOT NULL,            -- Duración en minutos
+    Clasificacion NVARCHAR(50) NOT NULL -- Clasificación de la película
+);
